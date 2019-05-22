b305c3481da72a98348a7c1c89f8cab02ac50942
yucheng <yujun04072010@163.com>
2019-05-22 18:33:55 +0800

    Initial commit

---
+++ src/main/webapp/medias/js/template/menu.js
@@ -0,0 +1,17 @@
+Vue.component('pagecomp', {
+    props:['menu'],
+    template: '#template-page'
+})
+Vue.component('menucomp', {
+    props:['menu'],
+    template: '#template-menu'
+})
+// boot up the demo
+var menu = new Vue({
+    el: '#index',
+    data: {
+        curMenu:null,
+        component: "pagecomp",
+        menus: [{name: "主页1",comp:"pageComp"}, {name: "主页2",comp:"menuComp"}, {name: "主页3",comp:"pageComp"}]
+    }
+})
