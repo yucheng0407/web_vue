ff0323b9b504e24660ffc29480cb2d7164d18099
yucheng <yujun04072010@163.com>
2019-05-24 00:13:38 +0800

    6868767

--- src/main/webapp/medias/js/template/menu.js
+++ src/main/webapp/medias/js/template/menu.js
@@ -1,17 +1,27 @@
-Vue.component('pagecomp', {
-    props:['menu'],
-    template: '#template-page'
-})
-Vue.component('menucomp', {
-    props:['menu'],
-    template: '#template-menu'
-})
 // boot up the demo
-var data=[{name: "主页1",comp:"pagecomp",subMenus:[{name: "主页1-2",comp:"menucomp"}, {name: "主页1-3",comp:"pagecomp"}]}, {name: "主页2",comp:"menucomp"}, {name: "主页3",comp:"pagecomp"}];
-var menu = new Vue({
+var data = [{
+    name: "主页1",
+    comp: "pagecomp",
+    subMenus: [{name: "主页1-1", comp: "menucomp"}, {name: "主页1-2", comp: "pagecomp"}, {name: "主页1-3", comp: "pagecomp"}]
+}, {name: "主页2",subMenus: [{name: "主页2-1", comp: "menucomp"}], comp: "menucomp"}, {name: "主页3" ,subMenus: [{name: "主页3-1", comp: "menucomp"}],comp: "pagecomp"}];
+var Vmenu = new Vue({
     el: '#index',
     data: {
-        curMenu:data[0],
+        parentName:null,
+        compMenu: data[0].subMenus,
+        curMenu: data[0],
         menus: data
+    },
+    components: {
+        pageComp: {
+            props: ['compMenu', 'curMenu','curCompMenu'],
+            template: '#template-page',
+            methods: {
+                updateP: function (menu, compMenu) {
+                    this.$emit("update:curCompMenu", compMenu)
+                    this.$emit("update:curMenu", menu)
+                }
+            }
+        }
     }
 })
