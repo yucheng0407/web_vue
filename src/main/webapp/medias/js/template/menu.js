c10be75eeea4b0991f356c2267808313a057936d
yucheng <yujun04072010@163.com>
2019-05-24 17:38:45 +0800

    样式修改

--- src/main/webapp/medias/js/template/menu.js
+++ src/main/webapp/medias/js/template/menu.js
@@ -2,24 +2,46 @@
 var data = [{
     name: "主页1",
     comp: "pagecomp",
-    subMenus: [{name: "主页1-1", comp: "menucomp"}, {name: "主页1-2", comp: "pagecomp"}, {name: "主页1-3", comp: "pagecomp"}]
-}, {name: "主页2",subMenus: [{name: "主页2-1", comp: "menucomp"}], comp: "menucomp"}, {name: "主页3" ,subMenus: [{name: "主页3-1", comp: "menucomp"}],comp: "pagecomp"}];
+    url: "1",
+    subMenus: [{name: "主页1-1", comp: "menucomp",url:"1-1"}, {name: "主页1-2", comp: "pagecomp"}, {name: "主页1-3", comp: "pagecomp"}]
+}, {name: "主页2", subMenus: [{name: "主页2-1", comp: "menucomp",url: "12321"}], comp: "menucomp"}, {
+    name: "主页3",
+    comp: "pagecomp"
+}];
 var Vmenu = new Vue({
     el: '#index',
     data: {
-        parentName:null,
-        compMenu: data[0].subMenus,
-        curMenu: data[0],
+        curData: {
+            compMenu: data[0],
+            curMenu: data[0]
+        },
         menus: data
     },
+    methods: {
+        goTo: function (menu) {
+            function getUrl(data, type) {
+                if (data.url) {
+                    return data
+                } else if (data[type]) {
+                    return getUrl((data[type])[0])
+                }
+                return data;
+            }
+            this.curData.compMenu = menu;
+            this.curData.curMenu = getUrl(menu, "subMenus");
+        }
+    }
+    ,
     components: {
         pageComp: {
-            props: ['compMenu', 'curMenu','curCompMenu'],
+            props: ['compMenu', 'pCurData'],
             template: '#template-page',
             methods: {
                 updateP: function (menu, compMenu) {
-                    this.$emit("update:curCompMenu", compMenu)
-                    this.$emit("update:curMenu", menu)
+                    this.$emit("update:pCurData", {
+                        compMenu: compMenu,
+                        curMenu: menu
+                    })
                 }
             }
         }
